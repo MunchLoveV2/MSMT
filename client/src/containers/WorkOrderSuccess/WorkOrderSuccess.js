0086ca0f08b4507f5e8eb446fcc5e0b1d68a6a50
uglymushroom <lifanjin@ucla.edu>
2018-10-31 19:52:48 -0700

    working progressbar

--- client/src/containers/WorkOrderSuccess/WorkOrderSuccess.js
+++ client/src/containers/WorkOrderSuccess/WorkOrderSuccess.js
@@ -1,24 +1,26 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
-import {connect} from "react-redux";
-import {withRouter} from "react-router-dom";
-
-
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import ProgressBar from "../../components/ProgressBar/ProgressBar.js";
 
 class WorkOrderSuccess extends Component {
-    
-    render () {
-
-        return (
-
-            <Aux>
-                <p> Sucess! Thank you for submitting work order </p>
-            </Aux>
-        );
-    }
-
+  state = {
+    percentage: 10
+  };
+  render() {
+    return (
+      <Aux>
+        <p> Sucess! Thank you for submitting work order </p>
+        <ProgressBar percentage={this.state.percentage} />
+      </Aux>
+    );
+  }
 }
 
-
-
-export default withRouter(connect(null, null)(WorkOrderSuccess));
+export default withRouter(
+  connect(
+    null,
+    null
+  )(WorkOrderSuccess)
+);
