303c741adfc76f8110bb04310177a24dd5e81c0a
Tony <tony.li.feng@gmail.com>
2018-11-06 18:43:07 -0800

    lll

--- server.js
+++ server.js
@@ -53,13 +53,13 @@ const db = require("./server/models");
 // routes
 const authRoute = require("./server/routes/auth.js")(app, passport);
 
-if (process.env.NODE_ENV === "production") {
+/* if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 app.get("*", (request, response) => {
-  response.sendFile(path.join(__dirname, "client1/build", "index.html"));
-});
+  response.sendFile(path.join(__dirname, "client/build", "index.html"));
+}); */
 
 require("./server/routes/apiRoutes.js")(app, db.Workorders, path);
 require("./server/routes/permissionRoutes.js")(app, db.userPermissions);
