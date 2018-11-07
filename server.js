4cd1188d8ff1dc1842918aa006d7afac4d4dc91e
Tony <tony.li.feng@gmail.com>
2018-11-06 18:05:50 -0800

    lol

--- server.js
+++ server.js
@@ -53,8 +53,9 @@ const db = require("./server/models");
 // routes
 const authRoute = require("./server/routes/auth.js")(app, passport);
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "./", "client", "build", "index.html"));
+app.get("*", function(req, res) {
+  const index = path.join(__dirname, "build", "index.html");
+  res.sendFile(index);
 });
 
 require("./server/routes/apiRoutes.js")(app, db.Workorders, path);
